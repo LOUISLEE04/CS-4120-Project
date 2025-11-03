7a9393dc0269c944509c020499fb16a60cd0a983
Isaac <ijcampbell10027@upei.ca>
2025-11-02 20:20:50 -0400

    updating Naive bayes

--- src/train_baselines.py
+++ src/train_baselines.py
@@ -1,9 +1,15 @@
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 import mlflow
 import mlflow.sklearn
+from pathlib import Path
 
 from data import load_data, clean_data, split_data
 import features
+
+# --- Set mlruns path ---
+mlruns_path = str(Path().resolve().parent / "mlruns")
+print(mlruns_path)
+mlflow.set_tracking_uri(mlruns_path)
 
 # ----- CLASSIFICATION BASELINES -----
 def load_and_split_classification_data():
@@ -22,10 +28,12 @@ def run_naive_bayes(var_smoothing, upsample: bool):
     if upsample:
         balance="balanced"
         X_train, y_train = features.upsample(X_train, y_train)
+    else:
+        balance="imbalanced"
 
-    with mlflow.start_run(run_name=f"GaussianNB_var{var_smoothing}_imbalanced"):
+    with mlflow.start_run(run_name=f"GaussianNB_var{var_smoothing}_{balance}"):
 
-        gnb = GaussianNB(var_smoothing=1e-12)
+        gnb = GaussianNB(var_smoothing=var_smoothing)
         mlflow.log_param("model_type", "GaussianNB")
         # - Fit and Predict -
         gnb.fit(X_train, y_train)
@@ -39,6 +47,13 @@ def run_naive_bayes(var_smoothing, upsample: bool):
         mlflow.log_metric("roc_auc", roc_auc)
         mlflow.sklearn.log_model(gnb, "model")
 
+        print(f"--- GaussianNB_var{var_smoothing}_{balance} ---")
         print(f"F1 Score: {f1:.3f}")
         print(f"ROC AUC: {roc_auc:.6f}")
 
+def tune_naive_bayes(var_smoothing: list):
+    for value in var_smoothing:
+        run_naive_bayes(value, False)
+        run_naive_bayes(value, True)
+
+tune_naive_bayes([1e-12])
